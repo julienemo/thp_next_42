f72c96ce46d073aa341581ef1f05674c41982a2a
julienemo <juliette.nada@gmail.com>
2020-06-02 19:16:55 +0200

    [add] session and registration controller - now user CRUD is ok (create user through registration only) - there is no token envolved at any stage of usage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::API
+  before_action :sanitize_devise_params, if: :devise_controller?
+  respond_to :json
+  
+  def sanitize_devise_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username])
+  end
+
+  def render_resource(resource)
+    if resource.errors.empty?
+      render json: resource
+    else
+      validation_error(resource)
+    end
+  end
+
+  def validation_error(resource)
+    render json: {
+      errors: [
+        {
+          status: '400',
+          title: 'Bad Request',
+          detail: resource.errors,
+          code: '100'
+        }
+      ]
+    }, status: :bad_request
+  end
 end

a8d2b6d0d1ddd43256270d7444b573562c33f083
julienemo <juliette.nada@gmail.com>
2020-06-05 21:07:51 +0200

    [fixup] cors initializer indentation

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,8 +2,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
       origins '*'
       resource '*',
-        headers: :any,
-        expose: ['Authorization'],
-        methods: :any
+      headers: :any,
+      expose: ['Authorization'],
+      methods: :any
  end
 end
